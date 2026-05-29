2e46a6fcd0544c1ef61716325ff9d050a96f8530
Richard Wai <richard@annexi-strayline.com>
2026-05-28 22:36:05 -0400

    Add vt220 font as a submodule and bundled font

--- app/fontmanager.cpp
+++ app/fontmanager.cpp
@@ -378,6 +378,13 @@ void FontManager::populateBundledFonts()
         1.0,
         32,
         false);
+    addBundledFont(
+        "Glass_TTY_VT220"
+        "VT220",
+        ":/fonts/vt220/Glass_TTY_VT220.ttf",
+        1.0,
+        32,
+        false);
 }
 
 void FontManager::addBundledFont(const QString &name,
